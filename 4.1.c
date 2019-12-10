8b84bb5916ef767c3217b1a53d1ce221d3ce034b
CRASHFIRE-666 <54841369+CRASHFIRE-666@users.noreply.github.com>
2019-12-10 22:08:31 +0300

    Create 4.1.c

---
+++ 4.1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 5
+
+int main()
+{
+	int arr[SIZE];
+	int a;
+	for (int i = 0; i < SIZE; i++)
+	{
+		scanf("%d", &a);
+		arr[i] = a;
+	}
+	int temp;
+	for (int i = 0; i < SIZE - 1; i++)
+	{
+		for (int j = 0; j < SIZE - 1; j++)
+		{
+			if (arr[j] < arr[j + 1])
+			{
+				temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+
+	for (int i = 0; i < SIZE; i++) printf("%d ", arr[i]);
+	
+	return 0;
+}
